c1e675cf0e0af12e7ec72aeff37e932f58f73787
optimist19 <oluwasegunbamidele3@gmail.com>
2022-03-29 06:49:04 -0700

    C - Even more pointers, arrays and strings 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,12 @@
  * Return: a pointer to the memory area @s
  */
 
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
 	}
